a3878a0b98fde283ea5f6a5c2f0830f49079584c
fredtom23 <fred7francis@gmail.com>
2022-01-30 22:52:55 +0530

    added Navbar

---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const Nav = () => {
+    return (
+        <Navbar expand="lg" variant="light" bg="light">
+            <Navbar.Brand>Resume Builder</Navbar.Brand>
+        </Navbar>
+    );
+};
+
+export default Nav;
